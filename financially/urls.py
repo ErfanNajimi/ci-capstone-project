70d205516a71e2bcfb26ec4e827e4220dd03dc03
Erfan Najimi <erfannajimi123@gmail.com>
2024-08-14 12:49:42 +0000

    Add styling to login page

--- financially/urls.py
+++ financially/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('tracker/', include("tracker.urls"), name="tracker-urls"),
     path('', include("public.urls"), name="public-urls")
