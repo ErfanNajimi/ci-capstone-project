cb75cac30934884792645e7aa063dda5703d33b1
Erfan Najimi <erfannajimi123@gmail.com>
2024-09-03 10:47:00 +0000

    Create new branch for second sprint

--- financially/settings.py
+++ financially/settings.py
@@ -29,10 +29,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == '1'
 
 ALLOWED_HOSTS = [
-    '8000-erfannajimi-cicapstonep-v1klmcfovrq.ws.codeinstitute-ide.net',
+    '8000-erfannajimi-cicapstonep-ujo5y5788tt.ws.codeinstitute-ide.net',
     '.herokuapp.com',
 ]
 
